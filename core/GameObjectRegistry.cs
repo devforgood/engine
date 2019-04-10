09febd60396e84980215d2733a07f17a1023da97
송준호 <junho@433.co.kr>
2019-04-10 21:00:39 +0900

    world

---
+++ core/GameObjectRegistry.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using uint32_t = System.UInt32;
+
+namespace core
+{
+    public class GameObjectRegistry
+    {
+        public delegate GameObject GameObjectCreationFunc();
+
+        /// <summary>
+        /// Global instance of GameObjectRegistry
+        /// </summary>
+        public static GameObjectRegistry sInstance = new GameObjectRegistry();
+
+        public static void StaticInit()
+        {
+            sInstance = new GameObjectRegistry();
+        }
+
+
+        public void RegisterCreationFunction(uint32_t inFourCCName, GameObjectCreationFunc inCreationFunction)
+        {
+            mNameToGameObjectCreationFunctionMap[inFourCCName] = inCreationFunction;
+        }
+
+        public GameObject CreateGameObject(uint32_t inFourCCName)
+        {
+            //no error checking- if the name isn't there, exception!
+            GameObjectCreationFunc creationFunc = mNameToGameObjectCreationFunctionMap[inFourCCName];
+
+            var gameObject = creationFunc();
+
+            //should the registry depend on the world? this might be a little weird
+            //perhaps you should ask the world to spawn things? for now it will be like this
+            World.sInstance.AddGameObject(gameObject);
+
+            return gameObject;
+
+        }
+
+
+	    private GameObjectRegistry()
+        {
+
+        }
+
+        Dictionary<uint32_t, GameObjectCreationFunc> mNameToGameObjectCreationFunctionMap;
+    }
+}
---
+++ core/World.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace core
+{
+    public class World
+    {
+        List<GameObject> mGameObjects;
+
+
+        /// <summary>
+        /// Global instance of GameObjectRegistry
+        /// </summary>
+        public static World sInstance = new World();
+
+        public static void StaticInit()
+        {
+            sInstance = new World();
+        }
+
+        public void AddGameObject(GameObject inGameObject)
+        {
+            mGameObjects.Add(inGameObject);
+            inGameObject.SetIndexInWorld(mGameObjects.Count - 1);
+
+        }
+        public void RemoveGameObject(GameObject inGameObject)
+        {
+            int index = inGameObject.GetIndexInWorld();
+
+            int lastIndex = mGameObjects.Count - 1;
+            if (index != lastIndex)
+            {
+                mGameObjects[index] = mGameObjects[lastIndex];
+                mGameObjects[index].SetIndexInWorld(index);
+            }
+
+            inGameObject.SetIndexInWorld(-1);
+
+            mGameObjects.RemoveAt(lastIndex);
+        }
+
+
+        public void Update()
+        {
+            //update all game objects- sometimes they want to die, so we need to tread carefully...
+
+            for (int i = 0, c = mGameObjects.Count; i < c; ++i)
+            {
+                GameObject go = mGameObjects[i];
+
+
+                if (!go.DoesWantToDie())
+                {
+                    go.Update();
+                }
+                //you might suddenly want to die after your update, so check again
+                if (go.DoesWantToDie())
+                {
+                    RemoveGameObject(go);
+                    go.HandleDying();
+                    --i;
+                    --c;
+                }
+            }
+        }
+
+        public List<GameObject> GetGameObjects() { return mGameObjects; }
+
+
+        World()
+        {
+
+        }
+
+
+    }
+}
