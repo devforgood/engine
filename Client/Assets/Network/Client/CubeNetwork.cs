1efee9f43d92439ebb8a12f03b2083697cfb82da
Denny <Denny@DENNY-PC>
2019-05-09 20:42:35 +0900

    오브젝트 회전및 애니메이션 처리

--- Client/Assets/Network/Client/CubeNetwork.cs
+++ Client/Assets/Network/Client/CubeNetwork.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class CubeNetwork : MonoBehaviour {
 
     public CActor actor = null;
+    public Animator animator;
 
 	// Use this for initialization
 	void Start () {
@@ -16,12 +17,31 @@ public class CubeNetwork : MonoBehaviour {
     {
         if(actor != null)
         {
-            transform.position = new Vector3(actor.GetLocation().mX, actor.GetLocation().mY, actor.GetLocation().mZ);
+            var last_position = transform.position;
+            transform.position = new Vector3(actor.GetLocation().mX, actor.GetLocation().mZ, actor.GetLocation().mY);
+
+            if(last_position != transform.position)
+            {
+                animator.SetFloat("Speed", 1);
+            }
+            else
+            {
+                animator.Play("Locomotion");
+            }
+
             //if(actor.IsLocalPlayer() == false)
-                //Debug.Log("Draw Remote Client Location : " + actor.GetLocation() + ", player_id : " + actor.GetPlayerId());
+            //Debug.Log("Draw Remote Client Location : " + actor.GetLocation() + ", player_id : " + actor.GetPlayerId());
 
+            if (actor.mDirection.mX == 0.0f && actor.mDirection.mY == 0.0f)
+            {
 
-            if(actor.IsLocalPlayer())
+            }
+            else
+            {
+               transform.rotation = Quaternion.LookRotation(new Vector3(actor.mDirection.mX, 0, actor.mDirection.mY));
+            }
+
+            if (actor.IsLocalPlayer())
             {
                 if (Input.GetKeyUp(KeyCode.R))
                 {
