c161054d03e396aad9c9efbedde7b23f88fd79fb
Denny <Denny@DENNY-PC>
2019-06-03 20:00:20 +0900

    게임 모드 추가

---
+++ Client/Assets/Network/core/GameMode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace core
+{
+    /// <summary>
+    /// 게임 모드는 게임 승패에 대한 룰을 정한다.
+    /// 예) 팀데스, 프리포올 등 
+    /// </summary>
+    class GameMode
+    {
+    }
+}
