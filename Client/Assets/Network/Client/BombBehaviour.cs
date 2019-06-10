de45489fc190f51de997cedee9ab3c89fefd8a19
Denny <Denny@DENNY-PC>
2019-06-10 18:57:02 +0900

    클라이언트에서 폭탄 충돌처리 제거

--- Client/Assets/Network/Client/BombBehaviour.cs
+++ Client/Assets/Network/Client/BombBehaviour.cs
@@ -25,19 +25,9 @@ public class BombBehaviour : MonoBehaviour
     {
         for (int i = 1; i < 5 + 1; ++i)
         { //The 3 here dictates how far the raycasts will check, in this case 3 tiles far
-            Physics.Raycast(transform.position + new Vector3(0, .5f, 0), direction, out RaycastHit hit, i, levelMask); //Raycast in the specified direction at i distance, because of the layer mask it'll only hit blocks, not players or bombs
 
-            if (!hit.collider)
-            { // Free space, make a new explosion
-                GameObject go = Instantiate(explosionPrefab, transform.position + (i * direction), explosionPrefab.transform.rotation);
+            GameObject go = Instantiate(explosionPrefab, transform.position + (i * direction), explosionPrefab.transform.rotation);
 
-                //DestroyCheckDown(go.transform.position);
-            }
-            else
-            { //Hit a block, stop spawning in this direction
-                //DestroyBlock(hit);
-                break;
-            }
             yield return new WaitForSeconds(.05f); //Wait 50 milliseconds before checking the next location
         }
 
