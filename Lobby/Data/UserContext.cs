bff6980ca2d1d50a86215d305afe8866e7154897
Denny <Denny@DENNY-PC>
2019-06-25 14:31:07 +0900

    로비 서버와 mysql 연동

---
+++ Lobby/Data/UserContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lobby.Models
+{
+    public class UserContext : DbContext
+    {
+        public UserContext (DbContextOptions<UserContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Lobby.Models.User> User { get; set; }
+    }
+}
--- Lobby/Startup.cs
+++ Lobby/Startup.cs
@@ -8,8 +8,10 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Lobby.Models;
 
 namespace Lobby
 {
@@ -42,6 +44,13 @@ namespace Lobby
                 options.Filters.Add(new CustomExceptionFilterAttribute());
 
             });
+
+            services.AddDbContext<UserContext>(options =>
+                    options.UseMySQL(Configuration.GetConnectionString("UserContext")));
+
+            //services.AddDbContext<BookContext>(options =>
+            //        options.UseMySQL(Configuration.GetConnectionString("BookContext")));
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
