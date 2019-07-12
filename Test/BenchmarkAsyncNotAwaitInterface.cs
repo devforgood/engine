46a378078a916384989bc06eb599d9ebfdaa4bcf
Denny <Denny@DENNY-PC>
2019-07-12 15:54:27 +0900

    밴치 마킹 테스트

---
+++ Test/BenchmarkAsyncNotAwaitInterface.cs
@@ -0,0 +1,122 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    [RankColumn, MinColumn, MaxColumn, StdDevColumn, MedianColumn]
+    [ClrJob, CoreJob]
+    [HtmlExporter, MarkdownExporter]
+    [MemoryDiagnoser]
+    public class BenchmarkAsyncNotAwaitInterface
+    {
+        string context = "text context";
+        [Benchmark]
+        public int CompletedAwait()
+        {
+            var t = new CompletedAwaitTest();
+            var a = t.DoAsync(context);
+            a.Wait();
+            return t.Length;
+        }
+
+        [Benchmark]
+        public int Completed()
+        {
+            var t = new CompletedTest();
+            var a = t.DoAsync(context);
+            a.Wait();
+            return t.Length;
+        }
+
+        [Benchmark]
+        public int Pragma()
+        {
+            var t = new PragmaTest();
+            var a = t.DoAsync(context);
+            a.Wait();
+            return t.Length;
+        }
+
+        [Benchmark]
+        public int Yield()
+        {
+            var t = new YieldTest();
+            var a = t.DoAsync(context);
+            a.Wait();
+            return t.Length;
+        }
+
+        [Benchmark]
+        public int FromResult()
+        {
+            var t = new FromResultTest();
+            var t2 = t.DoAsync(context);
+            return t2.Result;
+        }
+
+        public interface ITestInterface
+        {
+            int Length { get; }
+            Task DoAsync(string context);
+        }
+
+        class CompletedAwaitTest : ITestInterface
+        {
+            public int Length { get; private set; }
+            public async Task DoAsync(string context)
+            {
+                Length = context.Length;
+                await Task.CompletedTask;
+            }
+        }
+
+        class CompletedTest : ITestInterface
+        {
+            public int Length { get; private set; }
+            public Task DoAsync(string context)
+            {
+                Length = context.Length;
+                return Task.CompletedTask;
+            }
+        }
+
+        class PragmaTest : ITestInterface
+        {
+            public int Length { get; private set; }
+#pragma warning disable 1998
+            public async Task DoAsync(string context)
+            {
+                Length = context.Length;
+                return;
+            }
+#pragma warning restore 1998
+        }
+
+        class YieldTest : ITestInterface
+        {
+            public int Length { get; private set; }
+            public async Task DoAsync(string context)
+            {
+                Length = context.Length;
+                await Task.Yield();
+            }
+        }
+
+        public interface ITestInterface2
+        {
+            Task<int> DoAsync(string context);
+        }
+
+        class FromResultTest : ITestInterface2
+        {
+            public async Task<int> DoAsync(string context)
+            {
+                var i = context.Length;
+                return await Task.FromResult(i);
+            }
+        }
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,15 @@
+﻿using BenchmarkDotNet.Running;
+using System;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<BenchmarkAsyncNotAwaitInterface>();
+
+
+        }
+    }
+}
